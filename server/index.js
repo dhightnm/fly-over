c11df34bfb5133c08379f54a1957b262879addd1
dhightnm <dhightnm@gmail.com>
2021-08-13 13:53:46 -0700

    test2

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const morgan = require('morgan');
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
 
 const app = express();
 app.use(cors());
